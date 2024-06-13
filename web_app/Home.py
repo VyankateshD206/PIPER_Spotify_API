db2250deb9bf6c71c8cc6aa73376217cfe963490
Vyankatesh Deshpande <141760278+VyankateshD206@users.noreply.github.com>
2024-06-13 16:30:37 +0530

    Update Home.py

--- web_app/Home.py
+++ web_app/Home.py
@@ -141,6 +141,7 @@ if 'access_token' not in st.session_state:
 
 if 'access_token' in st.session_state:
     st.write("#### Select a :red[page] from the :red[sidebar] to get started.")             
+st.header("Made by:- Vyankatesh Deshpande | Btech CSE IIT Jodhpur")
 st.image('web_app/spotify_image.png', caption='PIPER', use_column_width=True) 
 
 
